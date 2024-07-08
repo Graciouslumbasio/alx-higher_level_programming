d9f53b704e66d422ae686a8c7a393917f6636fda
Graciouslumbasio <lumbasiogracious@gmail.com>
2024-07-08 03:10:50 -0700

    0x07-python-test_driven_development

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-"""Defines a name-printing function."""
+"""
+A function that prints My name is <first name> <last name>
+"""
 
 
 def say_my_name(first_name, last_name=""):
-    """Print a name.
-    Args:
-        first_name (str): The first name to print.
-        last_name (str): The last name to print.
-    Raises:
-        TypeError: If either of first_name or last_name are not strings.
-    """
-    if not isinstance(first_name, str):
+    """function that print complete name"""
+    if type(first_name) != str or first_name == "":
         raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
+    if type(last_name) != str:
         raise TypeError("last_name must be a string")
+    """print(f"{My name is {first_name} {last_name}")"""
     print("My name is {} {}".format(first_name, last_name))
